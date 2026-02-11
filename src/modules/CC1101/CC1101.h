499d4f62599c1700f13cf786e255680fde38deaa
x42en <x42en@users.noreply.github.com>
2026-02-11 01:06:00 +0100

    fix: ESP32 compilation errors

--- src/modules/CC1101/CC1101.h
+++ src/modules/CC1101/CC1101.h
@@ -1026,7 +1026,7 @@ public:
     \param value The value that indicates which function to place on that pin. See chip datasheet for details.
     \returns \ref status_codes
   */
-  int16_t setDIOMapping(uint32_t pin, uint32_t value) override;
+  int16_t setDIOMapping(uint32_t pin, uint32_t value);
 
 #if !RADIOLIB_GODMODE && !RADIOLIB_LOW_LEVEL
 protected:
